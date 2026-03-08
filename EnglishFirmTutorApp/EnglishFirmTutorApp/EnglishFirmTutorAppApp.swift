f91b875f080cee08f428e0f42c333896c10512cb
Stefy <Stefy73@users.noreply.github.com>
2026-03-08 14:47:12 +1100

    Initial commit: setup SwiftUI iOS project

---
+++ EnglishFirmTutorApp/EnglishFirmTutorApp/EnglishFirmTutorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishFirmTutorAppApp.swift
+//  EnglishFirmTutorApp
+//
+//  Created by Stefy Thomas on 8/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct EnglishFirmTutorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EnglishFirmTutorApp/EnglishFirmTutorAppTests/EnglishFirmTutorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishFirmTutorAppTests.swift
+//  EnglishFirmTutorAppTests
+//
+//  Created by Stefy Thomas on 8/3/2026.
+//
+
+import Testing
+@testable import EnglishFirmTutorApp
+
+struct EnglishFirmTutorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
